ad39ab96f1978d481da40e2295f9aee0b4105b0d
eien1123 <55547758+eien1123@users.noreply.github.com>
2022-12-19 14:55:52 +0900

    Clean up and Relocation script files.

---
+++ Assets/@Script/06. Data/Player/CharacterQuestData.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class CharacterQuestData
+{
+    public event UnityAction<CharacterQuestData> OnChangeQuestData;
+
+    [Header("Quest")]
+    [SerializeField] private uint mainQuestPrograss;
+    [SerializeField] private List<QuestSaveData> questSaveList;
+    
+    public void Initialize()
+    {
+        mainQuestPrograss = Constants.CHARACTER_DATA_MAIN_QUEST_PROGRESS;
+        questSaveList = new List<QuestSaveData>();
+    }
+
+    public void UpdateMainQuestPrograss(Quest quest)
+    {
+        if (quest.QuestCategory == QUEST_CATEGORY.MAIN)
+        {
+            MainQuestPrograss = quest.QuestID;
+        }
+    }
+
+    #region Property
+    public uint MainQuestPrograss
+    {
+        get { return mainQuestPrograss; }
+        set
+        {
+            mainQuestPrograss = value;
+            OnChangeQuestData?.Invoke(this);
+        }
+    }
+    public List<QuestSaveData> QuestSaveList
+    {
+        get { return questSaveList; }
+        set
+        {
+            questSaveList = value;
+            OnChangeQuestData?.Invoke(this);
+        }
+    }
+    #endregion
+}
